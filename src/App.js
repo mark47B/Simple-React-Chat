dfa39ad6c1b9d4ea3956ae441f54d8f822dec7af
mark47B <windachae47@gmail.com>
2023-07-28 16:34:17 +0300

    first comit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import "./App.css";
+import { ChatEngine, getOrCreateChat } from "react-chat-engine";
+// import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
+import { useState } from "react";
+
+function App() {
+  const [username, setUsername] = useState("");
+
+  const createDirectChat = (creds) => {
+    getOrCreateChat(
+      creds,
+      { is_direct_chat: true, usernames: [username] },
+      () => setUsername("")
+    );
+  };
+
+  const renderChatForm = (creds) => {
+    return (
+      <div style={{ marginBottom: "20px", marginTop: "10px" }}>
+        <input
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <button
+          style={{
+            marginTop: "10px",
+            marginLeft: "10px",
+            padding: "5px 8px",
+            backgroundColor: "#1890FF",
+            borderRadius: "5px",
+            cursor: "pointer",
+            color: "white",
+            borderWidth: "0px",
+          }}
+          onClick={() => createDirectChat(creds)}
+        >
+          Create
+        </button>
+      </div>
+    );
+  };
+
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
+  return (
+    <div className="App">
+      <ChatEngine
+        height="100vh"
+        projectID="37752325-3f2d-4e0e-8445-a0096ce27719"
+        userName={localStorage.getItem("username")}
+        userSecret={localStorage.getItem("password")}
+        renderNewChatForm={(creds) => renderChatForm(creds)}
+      />
+    </div>
+  );
+}
+
+export default App;
